be53490aa4bd4961b3e8f6e0fbf2bd5ed92fd31f
erinlefeyijimi <feyijimi.erinle@datamellon.com>
2023-07-09 09:03:58 +0100

    created new pagess

--- src/components/data/ServicesData.js
+++ src/components/data/ServicesData.js
@@ -43,7 +43,7 @@ const ServicesData = [
   },
   {
     img: vector,
-    href: '/services/custom-servies',
+    href: '/services/custom-services',
     heading: "Custom Services",
     description:
       "In addition to our core offerings, we provide custom services tailored to your requirements. We understand that each client has unique needs, and we are committed to delivering personalized solutions. We are here to assist you with editing and proofreading, social media management, email newsletters, or any other content-related services.",
